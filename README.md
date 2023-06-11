f2d3c8db1ed52b1f8fd68c7e9304076e09f849ff
Richard Ma <richard.ma.19850509@gmail.com>
2023-06-11 21:29:50 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tool-clear-image-of-deleted-products
+
+## 安装
+1. 解压压缩包
+1. 确认系统装有python3环境，运行`python3 --version`显示python版本信息即为系统环境良好
+
+## 使用方法
+1. 准备好待删除的文件列表，即csv文件
+1. 记下图片uploads目录位置，例如/home/example.com/wp-contents/uploads
+1. 进入src目录
+1. 运行程序: `python3 clear_image_of_deleted_products.py csv文件位置 uploads目录位置`
+1. 如列出的文件不存在则会出现文字提示，如果文件存在则直接被删除，不会出现文字提示
---
+++ src/clear_image_of_deleted_products.py
@@ -0,0 +1,43 @@
+import os, sys
+import logging
+from datetime import datetime
+import app
+
+
+class App(app.App):
+    def usage(self):
+        super().usage() # call parent class usage method
+
+        print("python/python3 clear_image_of_deleted_products.py (csv_file_name) (upload_dir)")
+        print("csv_file_name like: /home/xxx/xxxxxx.csv")
+        print("upload_dir like: /home/www/xxx.com/upload")
+        print("command like: python3 clear_image_of_deleted_products.py ../data/wc-product-export-10-6-2023-1686380411240.csv /home/example.com/upload")
+
+    def process(self):
+        if len(sys.argv) < 3:
+            sys.exti(-1)
+    
+        keyword = 'uploads'
+
+        csv_filename, upload_dir = sys.argv[1], sys.argv[2]
+        #remove / from upload_dir tail
+        if upload_dir.endswith('/'):
+            upload_dir = upload_dir[:-1]
+        print(upload_dir)
+
+        data = self.readCsvToDict(csv_filename)
+        for line in data:
+            sku, images = line['SKU'], line['Images']
+            for image in images.split(','):
+                image = image.strip()
+                idx = image.find(keyword)
+                image = image[idx+len(keyword)+1:]
+                image_filename = os.path.join(upload_dir, image)
+                if os.path.exists(image_filename):
+                    os.remove(image_filename)
+                else:
+                    print("Cann't find {}".format(image_filename))
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
